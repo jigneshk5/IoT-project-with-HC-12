cbd94524c9179f6b81a8faff01ad86001b201ebd
jignesh kumar <36167650+coding-killer@users.noreply.github.com>
2018-11-03 15:40:02 +0530

    Add files via upload

---
+++ hc-12_arduino.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+long timer=0;
+
+SoftwareSerial HC12(10, 9); // HC-12 TX Pin, HC-12 RX Pin
+void setup() {
+  Serial.begin(9600);             // Serial port to computer
+  HC12.begin(9600);               // Serial port to HC12
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.setGyroOffsets(-0.78,-1.56,-0.13);
+}
+void loop() {
+  mpu6050.update();
+  int x= mpu6050.getAngleX();
+  Serial.println(x);
+  while (x>0 && x<=180) {      // servo can rotate from 0 to 180
+    mpu6050.update();
+    x= mpu6050.getAngleX();
+    Serial.print("Roll angle ");
+    Serial.println(x);
+    if(millis() - timer > 2000){   //Position will be sent every 2  sec after the readings settle down 
+      HC12.write(x);      // Send that data to HC-12
+      timer= millis();
+    }
+  }
+}
