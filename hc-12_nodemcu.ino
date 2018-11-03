be20bf52bd894fbd4b48873a5457282ca19b6ca4
jignesh kumar <36167650+coding-killer@users.noreply.github.com>
2018-11-03 15:38:18 +0530

    Add files via upload

---
+++ hc-12_nodemcu.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+#include <Servo.h> 
+ 
+Servo myservo;
+SoftwareSerial HC12(4,5); // HC-12 TX Pin, HC-12 RX Pin
+void setup() {
+  Serial.begin(9600);             // Serial port to computer
+  myservo.attach(2);               // D4 to control signal
+  HC12.begin(9600);               // Serial port to HC12
+}
+void loop() {
+  int pos;
+  while (HC12.available()) {        // If HC-12 has data
+    pos = HC12.read();
+    Serial.print("Position: ");
+    Serial.println(pos);
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  }
+}
